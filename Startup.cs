323760163ac7e8e6f49ae48d95e37a299cdcbee9
vyshnivskyivlad <vyshnivskyivlad@gmail.com>
2022-04-12 17:19:25 +0300

    Добавьте файлы проекта.

---
+++ Startup.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:SportsStoreProducts:ConnectionString"]));
+            services.AddMvc(x => x.EnableEndpointRouting = false);
+            services.AddTransient<IProductRepository, EFProductRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "pagination",
+                    template: "Products/Page{productPage}",
+                    defaults: new { Controller = "Product", action = "List" });
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Product}/{action=List}/{id?}");
+            });
+
+            SeedData.EnsurePopulated(app);
+        }
+    }
+}
